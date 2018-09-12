604746079e58b9c8becd3fe260a3d32a27609393
dansbands <dansbands@hotmail.com>
2018-09-12 15:42:25 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Star Wars</h1>
+        </header>
+        <p className="App-intro">
+          Choose a character
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
