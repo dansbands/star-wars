d167ec92f6eae0ab85ea4a622eb097f33824a17f
dansbands <dansbands@hotmail.com>
2019-04-18 16:38:04 -0400

    more specific comments

--- src/utils/index.js
+++ src/utils/index.js
@@ -19,7 +19,7 @@ export const getFilm = (film) => {
 // The directions explicitly say to only use the endpoints in characters.json
 // If we wanted to fix this, and only had those endpoints available,
 // we could use the api's query endpoint, as shown below,
-// and call it in the error handling, as shown above, commented out.
+// and call it in the error handling, as shown above on line 9, commented out.
 // This would alleviate any pain points for the user, who knows that Obi Wan appeared in Episodes I-VI
 
 // export const findPerson = (person) => {
